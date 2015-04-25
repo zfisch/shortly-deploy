97af60da1ae1ddafb3078fbd1641cf2af89af12a
Zack Fischmann <zacharyrobert@gmail.com>
2015-04-25 16:08:11 -0700

    update app config for mongoose

--- app/config.js
+++ app/config.js
@@ -1,45 +1,26 @@
-var Bookshelf = require('bookshelf');
-var path = require('path');
+var mongoose = require('mongoose');
+mongoURI = process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://localhost/shortlydb';
+mongoose.connect(mongoURI);
 
-var db = Bookshelf.initialize({
-  client: 'sqlite3',
-  connection: {
-    host: '127.0.0.1',
-    user: 'your_database_user',
-    password: 'password',
-    database: 'shortlydb',
-    charset: 'utf8',
-    filename: path.join(__dirname, '../db/shortly.sqlite')
-  }
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log('Mongo is now open!');
 });
 
-db.knex.schema.hasTable('urls').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('urls', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('base_url', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
+  // link.increments('id').primary();
+  // link.string('url', 255);
+  // link.string('base_url', 255);
+  // link.string('code', 100);
+  // link.string('title', 255);
+  // link.integer('visits');
 
-db.knex.schema.hasTable('users').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
+  // user: 'your_database_user',
+  // password: 'password',
+
+  // user.increments('id').primary();
+  // user.string('username', 100).unique();
+  // user.string('password', 100);
 
 module.exports = db;
